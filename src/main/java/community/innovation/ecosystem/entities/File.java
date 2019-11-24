fd0ebec3195bc12e6ac76c2df70185ffe70ad59e
chow029 <rinku.chowdhury@arvato.com>
2019-11-24 14:22:51 +0100

    this class has no impact in terms of current development

--- src/main/java/community/innovation/ecosystem/entities/File.java
+++ src/main/java/community/innovation/ecosystem/entities/File.java
@@ -1,5 +1,6 @@
 package community.innovation.ecosystem.entities;
 
+import lombok.Data;
 import org.bson.types.Binary;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,8 +9,8 @@ import org.springframework.stereotype.Component;
 import java.io.InputStream;
 
 // this class has no impact in terms of current development approach
+@Data
 @Component
-@Document(collection = "knowledge.file")
 public class File {
 
     @Id
@@ -22,59 +23,4 @@ public class File {
     public Binary file;
     public InputStream stream;
 
-    public String getKnowledgeFileId() {
-        return knowledgeFileId;
-    }
-
-    public void setKnowledgeFileId(String knowledgeFileId) {
-        this.knowledgeFileId = knowledgeFileId;
-    }
-
-    public String getFsFileId() {
-        return fsFileId;
-    }
-
-    public void setFsFileId(String fsFileId) {
-        this.fsFileId = fsFileId;
-    }
-
-    public String getKnowledgeId() {
-        return knowledgeId;
-    }
-
-    public void setKnowledgeId(String knowledgeId) {
-        this.knowledgeId = knowledgeId;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getFileType() {
-        return fileType;
-    }
-
-    public void setFileType(String fileType) {
-        this.fileType = fileType;
-    }
-
-    public Binary getFile() {
-        return file;
-    }
-
-    public void setFile(Binary file) {
-        this.file = file;
-    }
-
-    public InputStream getStream() {
-        return stream;
-    }
-
-    public void setStream(InputStream stream) {
-        this.stream = stream;
-    }
 }
