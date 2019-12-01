a76c359d200db5b3f22d965ea50272812abbad76
chow029 <rinku.chowdhury@arvato.com>
2019-12-01 18:17:29 +0100

    user credentials service for authentication

---
+++ src/main/java/community/innovation/ecosystem/entities/UserCredentials.java
@@ -0,0 +1,60 @@
+package community.innovation.ecosystem.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+public class UserCredentials implements UserDetails {
+
+    private String username;
+    private String password;
+    private Boolean verification;
+
+    public UserCredentials() { }
+
+    public UserCredentials(String username, String password, Boolean verification) {
+        this.username = username;
+        this.password = password;
+        this.verification=verification;
+    }
+
+    // needed when role will applicable
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return verification;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return verification;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return verification;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return verification;
+    }
+}
---
+++ src/main/java/community/innovation/ecosystem/services/UserCredentialService.java
@@ -0,0 +1,33 @@
+package community.innovation.ecosystem.services;
+
+import community.innovation.ecosystem.entities.Credential;
+import community.innovation.ecosystem.entities.UserCredentials;
+import community.innovation.ecosystem.repositories.CredentialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserCredentialService implements UserDetailsService {
+    private CredentialRepository credentialRepository;
+
+    @Autowired
+    public UserCredentialService(CredentialRepository credentialRepository) {
+        this.credentialRepository = credentialRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Credential userCredential= credentialRepository.findByUsername(username);
+
+        if(userCredential==null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        // returning only username, password and verification status: authorities will come while role implementation needed
+        return new UserCredentials(userCredential.getUsername(),userCredential.getPassword(),userCredential.getVerification());
+    }
+}
