593dcf0349f0c99bf97db7bf1d372109eb740b9e
Rinku <rinku.chowdhury.connected@gmail.com>
2019-12-22 10:17:42 +0100

    removed tag:  @EnableConfigurationProperties(FileStorageProperties.class)

--- src/main/java/community/innovation/ecosystem/InnoEcoApplication.java
+++ src/main/java/community/innovation/ecosystem/InnoEcoApplication.java
@@ -1,12 +1,9 @@
 package community.innovation.ecosystem;
 
-import community.innovation.ecosystem.services.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(FileStorageProperties.class)
 public class InnoEcoApplication {
 
     public static void main(String[] args) {
